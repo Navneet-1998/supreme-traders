9125df61e8ffd8861ea07f88c61f7521680a023d
Navneet-1998 <103152970+Navneet-1998@users.noreply.github.com>
2024-09-26 14:39:43 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,38 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  // Enable React plugin
+  plugins: [react()],
+
+  // Resolve aliases
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      '@': path.resolve(__dirname, 'src'), // Alias for src directory
+    },
   },
+
+  // Server configuration
   server: {
-    port: 3000
-  }
-})
+    port: 3000, // Run server on port 3000
+  },
+
+  // Build configuration
+  build: {
+    chunkSizeWarningLimit: 10000, // Set chunk size warning limit to 1 MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', '@mui/material'], // Move heavy libraries to separate chunks
+        },
+      },
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
+});
